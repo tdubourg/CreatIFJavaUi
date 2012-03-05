bc06dd8788685768867162e320d73ce2b62e95d5
Troll <trolldu26@gmail.com>
2012-03-05 16:36:19 +0100

    Web Project, the right one

---
+++ CreaWeb/src/java/Action.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author
+ * troll
+ */
+import 
+public abstract class Action {
+	protected 
+}
